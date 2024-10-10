81892fcbf29151ed124802c7c4747229f65ac51f
bt-koch <bela.koch@hotmail.com>
2024-10-10 14:44:43 +0200

    create class for llama model

--- models/llm.py
+++ models/llm.py
@@ -1,23 +1,24 @@
 import ollama
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
+class llama:
 
-def define_topic(text):
-    instruction = """
-    I will provide you with the content of a media article.
-    Can you classify the topic of the article?
-    Answer with a single keyword from the following selection: Economy, Sports, Weather, Culture, War in Ucraine
-    In your response, please provide the keyword and only the keyword, no further text is necessary.
-    The text might be in german, english or french.
-    Here the article:
-    """
-    response = ollama.chat(model="llama3.1", messages=[
-        {
-            "role": "user",
-            "content": instruction+"\n"+text
-        }
-    ])
-    return(response["message"]["content"])
+    def define_topic(text):
+        instruction = """
+        I will provide you with the content of a media article.
+        Can you classify the topic of the article?
+        Answer with a single keyword from the following selection: Economy, Sports, Weather, Culture, War in Ucraine
+        In your response, please provide the keyword and only the keyword, no further text is necessary.
+        The text might be in german, english or french.
+        Here the article:
+        """
+        response = ollama.chat(model="llama3.1", messages=[
+            {
+                "role": "user",
+                "content": instruction+"\n"+text
+            }
+        ])
+        return(response["message"]["content"])
 
 def finbert_german_sentiment(text, device):
     tokenizer = AutoTokenizer.from_pretrained("scherrmann/GermanFinBert_SC_Sentiment")
