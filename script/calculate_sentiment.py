ca304a4a1af354e014c3e2a2f48dbcfd151034f6
bt-koch <bela.koch@hotmail.com>
2024-10-18 13:58:22 +0200

    clean and implement parallelization

--- script/calculate_sentiment.py
+++ script/calculate_sentiment.py
@@ -4,30 +4,29 @@ if os.path.basename(os.getcwd()) == "script":
     os.chdir("../")
 sys.path.append(os.getcwd())
 
+import config.environvars as environvars
 import data.extract
 import models.llm
-import models.apply_models
-# import config.environvars as environvars
+from models.apply_models import calculate_sentiment
 
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
+
+from pandarallel import pandarallel
+pandarallel.initialize(progress_bar=True)
 
 print("Prepare data...")
 df = pd.read_csv(environvars.paths.path_swissdox+"swissdox.csv")
-# df = df.iloc[1234:1236]
-device = models.llm.select_device()
+device = -1 # to utilize parallelized computation, using "mps" this does not work
 query_input = data.extract.swissdox.query_inputs
 
 num_splits = int(np.ceil(df.shape[0] / 500))
 dfs = np.array_split(df, num_splits)
 
-tqdm.pandas()
-
 for i in range(0, len(dfs)):
     print("Estimate sentiment for chunk "+str(i+1)+" of "+str(len(dfs))+"...")
     temp = dfs[i]
-    temp["sentiment_score"] = temp.progress_apply(lambda x: models.apply_models.calculate_sentiment(
+    temp["sentiment_score"] = temp.parallel_apply(lambda x: calculate_sentiment(
         x,
         query_input=query_input,
         device=device,
@@ -35,5 +34,3 @@ for i in range(0, len(dfs)):
     ), axis=1)
     print("Sentiment estimation of chunk "+str(i+1)+" finished, save file...")
     temp.to_csv(environvars.paths.path_preprocessed+"sentiment_scores/chunk_"+str(i+1)+".csv", index=False, sep=";")
-
-
