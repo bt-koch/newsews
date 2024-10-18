99c5b77b3e4e65f803a13feb8697a04f02b4ba8d
bt-koch <bela.koch@hotmail.com>
2024-10-18 11:10:47 +0200

    reduce imports

--- models/apply_models.py
+++ models/apply_models.py
@@ -1,14 +1,13 @@
-import data.transform
-import models.llm
-
-import re
-import numpy as np
+from data.transform import preprocess
+from models.llm import finbert_german_sentiment
+from re import compile, search
+from numpy import nanmean
 
 def calculate_sentiment(article, query_input, device, model, silent=True):
     if not silent:
         print("Calculate sentiment for article "+article["content_id"])
     pattern = [i for i in query_input if i["query_name"] == article["query_bank"]][0]
-    pattern = re.compile(pattern["regex"])
+    pattern = compile(pattern["regex"])
 
     text = article["content"]
     text = text.split("</p>")
@@ -17,14 +16,14 @@ def calculate_sentiment(article, query_input, device, model, silent=True):
     text = [item for item in text if bool(re.search(pattern, item))]
 
     if (model == "finbert_german_sentiment"):
-        model_initialise = models.llm.finbert_german_sentiment.model_initialise()
-        result = [models.llm.finbert_german_sentiment.finbert_german_sentiment(
+        model_initialise = finbert_german_sentiment.model_initialise()
+        result = [finbert_german_sentiment.finbert_german_sentiment(
             item,
             tokenizer=model_initialise[0],
             model=model_initialise[1],
             device=device
         ) for item in text]
-        result = np.nanmean(result)
+        result = nanmean(result)
     else:
         print("No model called "+model+" available. Return None.")
         result = None
