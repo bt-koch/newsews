34fa50fac21fd981a9d47feb5f9396c2360a4059
bt-koch <bela.koch@hotmail.com>
2024-10-19 12:04:39 +0200

    implement chunkwise parallelized and add todo

--- script/topic_modelling.py
+++ script/topic_modelling.py
@@ -7,30 +7,33 @@ sys.path.append(os.getcwd())
 import config.environvars as environvars
 import models.llm
 
-import pickle
+import numpy as np
 import pandas as pd
-from tqdm import tqdm
+from pandarallel import pandarallel
+pandarallel.initialize(progress_bar=True, nb_workers=3)
 
-with open(environvars.paths.path_swissdox+"cs_sentiment_randomsample.pkl", "rb") as file:
-    df = pickle.load(file)
+df = pd.read_csv(environvars.paths.path_preprocessed+"estim_dataset.csv", sep=";")
+# TODO: here use dataset_clean instead, and i can remove all cols other than the neceessary ones
+# TODO: look if i can merge new identifier to translated_leads
 
 translated_leads = pd.read_csv(environvars.paths.path_preprocessed+"translated_leads.csv", sep=";")
 translated_leads = translated_leads[["content_id", "translated_text"]]
 
-df_topic = pd.merge(df, translated_leads, on="content_id", how="left")
-df_topic = df_topic[["content_id", "translated_text"]]
-df_topic = df_topic[~pd.isna(df_topic["translated_text"])]
+df = pd.merge(df, translated_leads, on="content_id", how="left")
+df = df[["content_id", "translated_text"]]
+df = df[~pd.isna(df["translated_text"])]
 
-tqdm.pandas()
+# num_splits = int(np.ceil(df.shape[0] / 500))
+num_splits = int(np.ceil(df.shape[0] / 5))
+dfs = np.array_split(df, num_splits)
 
-print("start with topic classification...")
+dfs = dfs[0:3]
 
 pipe = models.llm.finbert_english_topic.model_initialise()
-df_topic["topic"] = df_topic["translated_text"].progress_apply(lambda x: models.llm.finbert_english_topic.finbert_english_topic(pipe, x))
 
-print("topic classification finished! Save results...")
-
-result = pd.merge(df, df_topic, on="content_id", how="left")
-result.to_csv(environvars.paths.path_preprocessed+"cs_example.csv", sep=";", index=False)
-
-print("script finished!")
+for i in range(0, len(dfs)):
+    print("Estimate topic for chunk "+str(i+1)+" of "+str(len(dfs))+"...")
+    temp = dfs[i]
+    temp["topic"] = temp["translated_text"].parallel_apply(lambda x: models.llm.finbert_english_topic.finbert_english_topic(pipe, x))
+    print("\nTopic estimation of chunk "+str(i+1)+" finished, save file...")
+    temp.to_csv(environvars.paths.path_preprocessed+"topics/chunk_"+str(i+1)+".csv", index=False, sep=";")
