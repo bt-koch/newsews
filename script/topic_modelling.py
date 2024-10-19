d29c0853dada065c3037ab5ffa96fceaf524a9a0
bt-koch <bela.koch@hotmail.com>
2024-10-19 13:37:07 +0200

    adjust to new data flow

--- script/topic_modelling.py
+++ script/topic_modelling.py
@@ -10,26 +10,21 @@ import models.llm
 import numpy as np
 import pandas as pd
 from pandarallel import pandarallel
-pandarallel.initialize(progress_bar=True, nb_workers=3)
+pandarallel.initialize(progress_bar=True)
 
-df = pd.read_csv(environvars.paths.path_preprocessed+"estim_dataset.csv", sep=";")
-# TODO: here use dataset_clean instead, and i can remove all cols other than the neceessary ones
-# TODO: look if i can merge new identifier to translated_leads
+df = pd.read_csv(environvars.paths.path_preprocessed+"dataset_clean.csv", sep=";")
 
 translated_leads = pd.read_csv(environvars.paths.path_preprocessed+"translated_leads.csv", sep=";")
 translated_leads = translated_leads[["content_id", "translated_text"]]
 
 df = pd.merge(df, translated_leads, on="content_id", how="left")
-df = df[["content_id", "translated_text"]]
+df = df[["identifier", "translated_text"]]
 df = df[~pd.isna(df["translated_text"])]
 
-# num_splits = int(np.ceil(df.shape[0] / 500))
-num_splits = int(np.ceil(df.shape[0] / 5))
+num_splits = int(np.ceil(df.shape[0] / 500))
 dfs = np.array_split(df, num_splits)
 
-dfs = dfs[0:3]
-
-pipe = models.llm.finbert_english_topic.model_initialise()
+pipe = models.llm.finbert_english_topic.model_initialise(device=-1)
 
 for i in range(0, len(dfs)):
     print("Estimate topic for chunk "+str(i+1)+" of "+str(len(dfs))+"...")
