f801c6262adf14ae92437de0f109f431831b64f8
bt-koch <bela.koch@hotmail.com>
2024-10-24 10:55:39 +0200

    save sentiment score estimation result

--- script/create_datasets.py
+++ script/create_datasets.py
@@ -48,4 +48,5 @@ path = environvars.paths.path_preprocessed+"sentiment_scores"
 files = [f for f in os.listdir(path) if re.match(r"^chunk_\d{1,3}.csv$", f)]
 df = [pd.read_csv(os.path.join(path, f), sep=";") for f in files]
 df = pd.concat(df)
-df.to_csv(environvars.paths.path_preprocessed+"first_result_sentiment.csv", index=False, sep=";")
+df = df[["identifier", "query_bank", "sentiment_score"]]
+df.to_csv(environvars.paths.path_results+"sentiment_scores.csv", index=False, sep=";")
