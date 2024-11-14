79ed93a7fca645c72756a494680edfec56a7e412
bt-koch <bela.koch@hotmail.com>
2024-11-14 11:11:11 +0100

    add group info of banks and add european gsibs

--- ingest/extract.py
+++ ingest/extract.py
@@ -163,7 +163,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "credit\\s+suisse|\\bcs(-[a-z]+)?\\b|\\b(swx:)?csgn(:swx)?\\b",
-            "ric": "CSGN.S^F23"
+            "ric": "CSGN.S^F23",
+            "group": ["swissbank", "gsib_europe"]
         },
         {
             "query_name": "ubs",
@@ -175,7 +176,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "\\bubs(-[a-z]+)?\\b|\\b(swx:)?ubsg(:swx)?\\b",
-            "ric": "UBSG.S"
+            "ric": "UBSG.S",
+            "group": ["swissbank", "gsib_europe"]
         },
         # {
         #     "query_name": "arundel",
@@ -198,7 +200,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "b(a|â)loise|\\b(swx:)?baln(:swx)?\\b",
-            "ric": "BALN.S"
+            "ric": "BALN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_genf",
@@ -210,7 +213,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "banque\\s+cantonale\\s+\\s+de\\s+genev(e|è)ve|\\b(swx:)?bcge(-[a-z]+|:swx)?\\b|kantonalbank\\s+genf|genfer[a-z]*\\s+kantonalbank",
-            "ric": "BCGE.S"
+            "ric": "BCGE.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_jura",
@@ -222,7 +226,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "banque\\s+cantobale\\s+du\\s+jura|\\b(swx:)?bcj(-[a-z]+|:swx)?\\b|kantonalbank\\s+jura|jurassische[a-z]*\\s+kantonalbank",
-            "ric": "BCJ.S"
+            "ric": "BCJ.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_wallis",
@@ -234,7 +239,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "banque\\s+cantonale\\s+du\\s+valais|\\b(swx:)?wkbn(-[a-z]+|:swx)?\\b|kantonalbank\\s+wallis|walliser[a-z]*\\s+kantonalbank",
-            "ric": "WKB.S"
+            "ric": "WKB.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_waadt",
@@ -246,7 +252,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "banque\\s+cantonale\\s+du\\s+vaudoise|\\b(swx:)?bcvn(-[a-z]+|:swx)?\\b|kantonalbank\\s+waadt|waadtl(ä|ae)nder[a-z]*\\s+kantonalbank",
-            "ric": "BCVN.S"
+            "ric": "BCVN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_baselland",
@@ -258,7 +265,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "baselland[a-z]*\\s+kantonalbank|\\b(swx:)?blkb(-[a-z]+|:swx)?\\b|banque\\s+cantonale\\s+de\\s+b(â|a)le(-|\\s)campagne",
-            "ric": "BLKB.S"
+            "ric": "BLKB.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_baselstadt",
@@ -270,7 +278,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "basler[a-z]*\\s+kantonalbank|\\bbkb(-[a-z])?\\b|\\b(swx:)?bskp(:swx)?\\b|banque\\s+cantonale\\s+de\\s+b(â|a)le",
-            "ric": "BSKP.S"
+            "ric": "BSKP.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_bern",
@@ -282,7 +291,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "berner[a-z]*\\s+kantonalbank|\\bbekb(-[a-z]*)?\\b|\\bbcbe(-[a-z]*)?\\b|\\b(swx:)?bekn(-[a-z]*|:swx)?\\b|banque\\s+cantonale\\s+bernoise",
-            "ric": "BEKN.S"
+            "ric": "BEKN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "cembra_money_bank",
@@ -294,7 +304,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "cembra\\s+money\\s+bank|\\bcembra(-[a-z]*)\\b|\\b(swx:)?cmbn(-[a-z]*|:swx)?\\b",
-            "ric": "CMBN.S"
+            "ric": "CMBN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "efg_international",
@@ -306,7 +317,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "\\befg\\s+international|\\befg(-[a-z]*)\\b|\\b(swx:)?efgn(-[a-z]*|:swx)?\\b",
-            "ric": "EFGN.S"
+            "ric": "EFGN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_glarus",
@@ -318,7 +330,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "glarner[a-z]*\\s+kantonalbank|\\bglkb(-[a-z]*)?\\b|\\b(swx:)?glkbn(-[a-z]*|:swx)?\\b|banque\\s+cantonale\\s+de\\s+glaris",
-            "ric": "GLKBN.S"
+            "ric": "GLKBN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_graubuenden",
@@ -330,7 +343,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "graub(ü|ue)ndner[a-z]*\\s+kantonalbank|\\bgkb(-[a-z]*)\\b|\\b(swx:)?grkp(-[a-z]*|:swx)?|banque\\s+cantonale\\s+des\\s+grisons",
-            "ric": "GRKP.S"
+            "ric": "GRKP.S",
+            "group": "swissbank"
         },
         {
             "query_name": "hypothekarbank_lenzburg",
@@ -342,7 +356,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "hypothekarbank\\s+lenzburg|\\b(swx:)?hbln(-[a-z]*|:swx)?\\b",
-            "ric": "HBLN.S"
+            "ric": "HBLN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "julius_baer",
@@ -354,7 +369,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "julius\\s+b(ä|ae)r|\\b(swx:)?baer(-[a-z]*|:swx)?\\b",
-            "ric": "BAER.S"
+            "ric": "BAER.S",
+            "group": "swissbank"
         },
         {
             "query_name": "liechtensteinische_landesbank",
@@ -366,7 +382,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "liechtensteinisch[a-z]*\\s+landesbank|\\b(swx:)?llbn(-[a-z]*|:swx)?\\b|banque\\s+centrale\\s+du\\s+liechtenstein",
-            "ric": "LLBN.S"
+            "ric": "LLBN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_luzern",
@@ -378,7 +395,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "luzerner[a-z]*\\s+kantonalbank|\\blukb(-[a-z]*)?\\b|\\b(swx:)?lukn(-[a-z]*|:swx)?|banque\\s+cantonale\\s+de\\s+lucerne",
-            "ric": "LUKN.S"
+            "ric": "LUKN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_stgallen",
@@ -390,7 +408,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "(st\\.|sankt)\\s+galler[a-z]*\\s+kantonalbank|\\bsgkb(-[a-z]*)?\\b|\\b(swx:)?sgkn(-[a-z]*|:swx)?\\b|banque\\s+cantonale\\s+de\\s+saint(-|\\s+)gall",
-            "ric": "SGKN.S"
+            "ric": "SGKN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "swissquote",
@@ -402,7 +421,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "swissquote|\\b(swx:)?sqn(-[a-z]*|:swx)?\\b",
-            "ric": "SQN.S"
+            "ric": "SQN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_thurgau",
@@ -414,7 +434,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "thurgauer[a-z]*\\s+kantonalbank|\\btkb(-[a-z]*)?\\b|\\b(swx:)?tkbp(-[a-z]*|:swx)?\\b|banque\\s+cantonale\\s+de\\s+thurgovie",
-            "ric": "TKBP.S"
+            "ric": "TKBP.S",
+            "group": "swissbank"
         },
         {
             "query_name": "valartis",
@@ -426,7 +447,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "valartis|\\b(swx:)?vlrt(-[a-z]*|:swx)?\\b",
-            "ric": "VLRT.S"
+            "ric": "VLRT.S",
+            "group": "swissbank"
         },
         {
             "query_name": "valiant",
@@ -438,7 +460,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "valiant|\\b(swx:)?vatn(-[a-z]*|:swx)?\\b",
-            "ric": "VATN.S"
+            "ric": "VATN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "vontobel",
@@ -450,7 +473,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "vontobel|\\b(swx:)?vonn(-[a-z]*|:swx)?\\b",
-            "ric": "VONN.S"
+            "ric": "VONN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "vp_bank",
@@ -462,7 +486,8 @@ class swissdox:
             "from": default_date_from,
             "to": default_date_to,
             "regex": "\\bvp\\s+bank|\\b(swx:)?vpbn(-[a-z]*|:swx)?\\b",
-            "ric": "VPBN.S"
+            "ric": "VPBN.S",
+            "group": "swissbank"
         },
         {
             "query_name": "kantonalbank_zug",
@@ -475,7 +500,62 @@ class swissdox:
             "to": default_date_to,
             # here stock ticker might match a lot of unwanted substrings...
             "regex": "zuger[a-z]*\\s+kantonalbank|zugerkb(-[a-z]*)?|\\b(swx:)?zuger(-[a-z]*|:swx)?\\b|banque\\s+cantonale\\s+de\\s+zoug",
-            "ric": "ZUGER.S"
+            "ric": "ZUGER.S",
+            "group": "swissbank"
+        },
+        {
+            "query_name": "hsbc",
+            "regex": "hsbc|hsba",
+            "ric": "HSBA.L",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "barclays",
+            "regex": "barclays|\\bbarc\\b",
+            "ric": "BARC.L",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "bnp_paribas",
+            "regex": "\\bbnp\\b",
+            "ric": "BNPP.PA",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "deutsche_bank",
+            "regex": "\\bdeutsche\\bbank\\b|\\bdb(k)?\\b",
+            "ric": "DBKGn.DE",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "groupe_credit_agricole",
+            "regex": "\\bcr[eé]dit\\W+agricole\\b|\\bla\\W+banque\\W+verte\\b|\\baca\\b",
+            "ric": "CAGR.PA",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "ing",
+            "regex": "\\bing(a)?\\b",
+            "ric": "INGA.AS",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "santander",
+            "regex": "\\bsan(tander)?\\b",
+            "ric": "SAN.MC",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "societe_generale",
+            "regex": "\\bsociet[eé]\\W+g[eé]n[eé]rale\\b|\\bgle\\b",
+            "ric": "SOGN.PA",
+            "group": "gsib_europe"
+        },
+        {
+            "query_name": "standard_chartered",
+            "regex": "\\bstandard\\W+chartered\\b|\\bstan\\b",
+            "ric": "STAN.L",
+            "group": "gsib_europe"
         }
     ]
 
