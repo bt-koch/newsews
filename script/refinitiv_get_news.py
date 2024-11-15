dc0ea9992b5efc6e57527a653ff77b104d496346
bt-koch <bela.koch@hotmail.com>
2024-11-15 09:59:18 +0100

    adjustments to avoid reaching API limit

--- script/refinitiv_get_news.py
+++ script/refinitiv_get_news.py
@@ -9,29 +9,34 @@ import ingest.extract
 
 import pandas as pd
 import eikon as ek
+import time
 
 ingest.extract.refinitiv.connect()
 
-days = pd.date_range(start="2023-08-15", end="2024-11-13")
-
+days = pd.date_range(start="2023-09-01", end="2024-10-31")
 banks = [b["ric"] for b in ingest.extract.swissdox.query_inputs if "gsib_europe" in b["group"]]
-
 df = []
+calls = 0
 
 for bank in banks:
     print("\nGet news articles for "+bank)
     if bank == "CSGN.S^F23":
         continue
     for day in days.strftime('%Y-%m-%d'):
-        print(f"\rDay {day}", end='', flush=True)
         try:
+            calls = calls+1
             articles = ek.get_news_headlines(
-                query="R:"+bank+" and Language:LEN",
+                query="R:"+bank+" AND Language:LEN AND Topic:FINS AND Topic:SIGNWS NOT Source:PUBT",
                 date_from=day+"T00:00:00",
                 date_to=day+"T23:59:59",
                 count=100
             )
+            time.sleep(0.2)
+            if articles.shape[0] > 5:
+                articles = articles.sample(5)
             for i in range(0, articles.shape[0]):
+                calls = calls+1
+                print(f"\rDay {day}, number of calls: {calls}", end='', flush=True)
                 temp = pd.DataFrame([{
                     "bank": bank,
                     "date": articles.iloc[i]["versionCreated"],
@@ -39,6 +44,8 @@ for bank in banks:
                     "story_id": articles.iloc[i]["storyId"]
                 }])
                 df.append(temp)
+                calls = calls+1
+                time.sleep(0.2)
         except:
             print(" -> exception for "+day)
             temp = pd.DataFrame([{
