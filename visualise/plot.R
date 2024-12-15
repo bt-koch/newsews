81b934f15d3556edb051a62264dd2dd3fc97d52d
bt-koch <bela.koch@hotmail.com>
2024-12-15 14:11:53 +0100

    function to plot oirfs

---
+++ visualise/plot.R
@@ -0,0 +1,45 @@
+create_plot_irfs <- function(oirfs) {
+  
+  png(filename="tex/images/oirfs.png", width = 600, height = 400, res = 72)
+  
+  lwd = 1.5
+  lwd_grid = 0.75
+  
+  ymin <- min(c(oirfs$cds, oirfs$sentiment))
+  ymax <- max(c(oirfs$cds, oirfs$sentiment))
+
+  par(mfrow = c(2,2), oma = c(2,4,1,1) + 0.1, mar = c(1,0,1,1) + 0.1)
+  
+  plot(oirfs$cds[,1], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+       ylim = c(ymin, ymax))
+  grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
+  par(new = T)
+  plot(oirfs$cds[,1], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+       xaxt = "n", main = "cds on cds", lwd = lwd)
+  
+  plot(oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+       ylim = c(ymin, ymax))
+  grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
+  par(new = T)
+  plot(oirfs$cds[,2], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+       xaxt = "n", yaxt = "n", main = "cds on sentiment", lwd = lwd)
+  
+  plot(oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+       ylim = c(ymin, ymax))
+  grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
+  par(new = T)
+  plot(oirfs$sentiment[,1], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+       main = "sentiment on cds", lwd = lwd)
+  
+  plot(oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+       ylim = c(ymin, ymax))
+  grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
+  par(new = T)
+  plot(oirfs$sentiment[,2], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+       yaxt = "n", main = "sentiment on sentiment", lwd = lwd)
+  
+  
+  
+  dev.off()
+  
+}
