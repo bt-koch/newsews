81cda200e7a29f20f6054516ebd833aa264b6168
bt-koch <bela.koch@hotmail.com>
2024-12-16 23:53:16 +0100

    fix xaxis

--- visualise/plot.R
+++ visualise/plot.R
@@ -7,35 +7,37 @@ create_plot_irfs <- function(oirfs) {
   
   ymin <- min(c(oirfs$cds, oirfs$sentiment))
   ymax <- max(c(oirfs$cds, oirfs$sentiment))
+  
+  xvals <- 0:(length(oirfs$cds[,1])-1)
 
   par(mfrow = c(2,2), oma = c(2,4,1,1) + 0.1, mar = c(1,0,1,1) + 0.1)
   
-  plot(oirfs$cds[,1], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+  plot(xvals, oirfs$cds[,1], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
        ylim = c(ymin, ymax))
   grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
   par(new = T)
-  plot(oirfs$cds[,1], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+  plot(xvals, oirfs$cds[,1], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
        xaxt = "n", main = "cds on cds", lwd = lwd)
   
-  plot(oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+  plot(xvals, oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
        ylim = c(ymin, ymax))
   grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
   par(new = T)
-  plot(oirfs$cds[,2], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+  plot(xvals, oirfs$cds[,2], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
        xaxt = "n", yaxt = "n", main = "cds on sentiment", lwd = lwd)
   
-  plot(oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+  plot(xvals, oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
        ylim = c(ymin, ymax))
   grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
   par(new = T)
-  plot(oirfs$sentiment[,1], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+  plot(xvals, oirfs$sentiment[,1], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
        main = "sentiment on cds", lwd = lwd)
   
-  plot(oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+  plot(xvals, oirfs$cds[,2], type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n",
        ylim = c(ymin, ymax))
   grid(nx = NULL, ny = NULL, lty = 2, col = "gray", lwd = lwd_grid)
   par(new = T)
-  plot(oirfs$sentiment[,2], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
+  plot(xvals, oirfs$sentiment[,2], type = "l", ylim = c(ymin, ymax), xlab = "", ylab = "",
        yaxt = "n", main = "sentiment on sentiment", lwd = lwd)
   
   
